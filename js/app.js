305b449ce82c788d69dd1e8a1f5734df52b76916
Cameron Pellett <bubionbreakfast@gmail.com>
2019-09-05 16:12:50 +0100

    now really MVP

--- js/app.js
+++ js/app.js
@@ -4,13 +4,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const handleFormSubmit = function (event) {
     event.preventDefault();
-    const resultParagraph = document.querySelector('#reading-list');
-    resultParagraph.textContent = `Your reading list is ${event.target.title.value}
+
+    const newReadingList = document.createElement('p');
+    const list = document.querySelector('ul')
+    newReadingList.textContent = `Your reading list is ${event.target.title.value}
     ${event.target.author.value} ${event.target.category.value}`;
+    // newReadingList.classList.add(ul)
+    list.appendChild(newReadingList);
+
     document.getElementById('new-item-form').reset();
   };
 
-console.log();
+
 
   const form = document.querySelector('#new-item-form');
   form.addEventListener('submit', handleFormSubmit);
